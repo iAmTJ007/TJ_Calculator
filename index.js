4b6196637ecb657776421b66619cf1f5ee4df4e1
iAmTj007 <eveninghappy11@gmail.com>
2023-01-18 15:33:56 +0530

    backspace working

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const number=document.querySelectorAll('[data-number]')
 const operand=document.querySelectorAll('[data-operand]')
+const equalskey=document.querySelector('[data-equal]')
+const backspace=document.querySelector('.delete')
+
+backspace.addEventListener('click',()=>{
+const up_disp=document.querySelector('.up')
+const sliced_disp=up_disp.textContent.slice(0,-1)
+if(up_disp.textContent.length>1)up_disp.textContent=sliced_disp 
+else if(up_disp.textContent.length==1)up_disp.textContent='0'  
+             
+
+})
 number.forEach((button)=>{
     button.addEventListener('click',()=>{
         up_display(button.textContent) //it will append in an up display for each number
