1737aa92bca396e83a62e2a593d18a0335d9e155
iAmTj007 <eveninghappy11@gmail.com>
2023-02-11 19:46:11 +0530

    backspace bug left

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ const number=document.querySelectorAll('[data-number]')
 const operand=document.querySelectorAll('[data-operand]')
 const equalskey=document.querySelector('[data-equal]')
 const backspace=document.querySelector('.delete')
-let op=0
-let n1=0
+let op=''
+let n1=""
 let n2=""
 backspace.addEventListener('click',()=>{     //slice from 1st to second last element of display and replace it
 const up_disp=document.querySelector('.up') //with original display such that last element is deleted
 const sliced_disp=up_disp.textContent.slice(0,-1) //but dont do it when length of display is 1
-if(up_disp.textContent.length>1)up_disp.textContent=sliced_disp 
+if(up_disp.textContent[-1]=='+' && up_disp.textContent.length>1){
+    op==''
+    up_disp.textContent=sliced_disp
+}
+else if(op!='' && up_disp.textContent.length>1){
+    n2=n2.slice(0,-1)
+    up_disp.textContent=sliced_disp
+}
+else if(op=='' && up_disp.textContent.length>1){
+    n1=n1.slice(0,-1)
+    up_disp.textContent=sliced_disp
+}
 else if(up_disp.textContent.length==1)up_disp.textContent='0'  
+
+
              
 
 })
 number.forEach((button)=>{
     button.addEventListener('click',()=>{
-        if(op!=0)  // if operator already used then it must be n2 so we store it as string
+        if(op!='')  // if operator already used then it must be n2 so we store it as string
         {
             n2=n2.concat(button.textContent)
         }
@@ -24,12 +37,12 @@ number.forEach((button)=>{
 })
 operand.forEach((button)=>{
     button.addEventListener('click',()=>{
-        if(op==0){      //if operator never used then obviously it is n1 so store it
+        if(op==''){      //if operator never used then obviously it is n1 so store it
         const up_container=document.querySelector('.up')
          n1=up_container.textContent
          op=button.textContent
         }
-        else if(op!=0){   // if operator used already then calculate previous 
+        else if(op!=''){   // if operator used already then calculate previous 
             let a=calculate(n1,op,n2)
             op=button.textContent
             n2=''
@@ -40,9 +53,9 @@ operand.forEach((button)=>{
 })
 equalskey.addEventListener('click',()=>{
     calculate(n1,op,n2)
-    n1=0
+    n1=""
     n2=''
-    op=0
+    op=''
 })
 
 
@@ -65,8 +78,8 @@ function calculate(n1,op,n2){
         return parseInt(n1)-parseInt(n2)
     }
     else if(op=='x'){
-      up_container.textContent=parseInt(n1)-parseInt(n2)
-      return parseInt(n1)-parseInt(n2)
+      up_container.textContent=parseInt(n1)*parseInt(n2)
+      return parseInt(n1)*parseInt(n2)
     }
     else if(op=='/'){
         up_container.textContent=parseInt(n1)/parseInt(n2)
